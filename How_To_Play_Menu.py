0f3aa8c848aa966546b5b0e55762728429d0f6d8
BootyMuncher23 <168749007+BootyMuncher23@users.noreply.github.com>
2024-05-17 10:10:03 -0400

    Create How_To_Play_Menu.py

---
+++ How_To_Play_Menu.py
@@ -0,0 +1,37 @@
+import pygame
+
+
+def how_to_play():
+    # Define some colors
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    SAND = (225, 198, 153)
+
+    board_image = pygame.image.load('How_To_Play_Menu.png').convert_alpha()
+
+    size = (700, 500)
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Tic-Tac-Toe!")
+
+    # Loop until the user clicks the close button.
+    done = False
+
+    # Used to manage how fast the screen updates
+    clock = pygame.time.Clock()
+
+    while not done:
+        # --- Main event loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        screen.blit(board_image, (0, 0))
+
+        # Update the screen with drawing.
+        pygame.display.flip()
+
+        # Limit to 60 frames per second
+        clock.tick(60)
+
+    # Close the window and quit.
+    pygame.quit()
