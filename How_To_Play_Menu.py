d97e1c15d36aeb719b4112f90b4ae3c6ace7a7ba
BootyMuncher23 <168749007+BootyMuncher23@users.noreply.github.com>
2024-05-17 10:23:12 -0400

    Update How_To_Play_Menu.py

--- How_To_Play_Menu.py
+++ How_To_Play_Menu.py
@@ -1,17 +1,16 @@
 import pygame
-
+from Button import Button
+from GameBoard import board_draw
 
 def how_to_play():
-    # Define some colors
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-    SAND = (225, 198, 153)
-
     board_image = pygame.image.load('How_To_Play_Menu.png').convert_alpha()
+    start_img = pygame.image.load('START_button.png').convert_alpha()
 
     size = (700, 500)
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption("Tic-Tac-Toe!")
+
+    start_button = Button(300, 430, start_img)
 
     # Loop until the user clicks the close button.
     done = False
@@ -26,6 +25,10 @@ def how_to_play():
                 done = True
 
         screen.blit(board_image, (0, 0))
+
+        if start_button.draw(screen):
+            done = True
+            board_draw()
 
         # Update the screen with drawing.
         pygame.display.flip()
