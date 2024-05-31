7dac9e929270adb2e7f3405d3aa27270027dab64
ArthurSevk12 <168748803+ArthurSevk12@users.noreply.github.com>
2024-05-31 10:03:08 -0400

    Update Button.py

--- Button.py
+++ Button.py
@@ -2,18 +2,21 @@ import pygame
 
 
 class Button():
+    # Lines 6 to 10
     def __init__(self, x, y, image):
         self.image = image
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
         self.clicked = False
 
+    # Line 13 is a method that calls the button to appear on the screen
     def draw(self, surface):
         action = False
-        # draw button on screen
 
+        # pos gets the mouse position
         pos = pygame.mouse.get_pos()
 
+        # the bottom if statements allows the button to have the action of being 'pressed' by the user
         if self.rect.collidepoint(pos):
             if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
                 self.clicked = True
@@ -22,6 +25,7 @@ class Button():
         if pygame.mouse.get_pressed()[0] == 0:
             self.clicked = False
 
+        # Line 29 allows the image to be displayed as a button
         surface.blit(self.image, (self.rect.x, self.rect.y))
 
         return action
