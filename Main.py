98982fa8aa3a45c0fe5a5248f6a56e1c62db3085
BootyMuncher23 <168749007+BootyMuncher23@users.noreply.github.com>
2024-06-07 10:16:27 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,17 @@
+'''
+-------------------------------------------------------------------------------
+Name:  tic_tac_toe.py
+Course: ICS4U (Grade 12 Computer Science)
+
+Purpose: To demonstrate learned programming concepts using Python in PyCharm,
+along with relevant modules (i.e. PyGame) to create a fully-functional
+game of Tic-Tac-Toe using the author's faces as markers.
+
+Author:   Lee. J, Severinets. A
+Created:  06/07/2024
+------------------------------------------------------------------------------
+'''
+
 from Menu import Menu
 
 # The bottom line calls the code to run
